3ab7b22a486aba36f9bc4dea32672410789a1c12
Jintae Joo <10439318+jtjoo@users.noreply.github.com>
2025-12-07 17:39:49 +0900

    server doesn't need browser

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
     open: '/index.html'
   },
   preview: {
-    allowedHosts: ['themilliways.com']
+    allowedHosts: ['themilliways.com'],
+    open: false
   }
 })
